69247485523f540be2e5f9885879b544d657179f
martijn <martijn.brug@gmail.com>
2020-12-28 12:22:49 +0100

    Removed foundatin script for test

--- _gridsome_template/src/main.js
+++ _gridsome_template/src/main.js
@@ -2,12 +2,13 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import config from 'shared-components/config';
 import DefaultLayout from '~/layouts/Default.vue'
+import {styles} from 'shared-components';
 
 export default function (Vue, {router, head, isClient}) {
     if (isClient) {
         console.log('Loading external scripts', isClient);
-        window.$ = require('shared-components/jquery.min');
-        window.Foundation = require('shared-components/foundation.min');
+/*        window.$ = require('shared-components/jquery.min');
+        window.Foundation = require('shared-components/foundation.min');*/
     }
     // Set default layout as a global component
     Vue.component('Layout', DefaultLayout);
