b5563968795e4a4c52d3bab0816ce0b7fa4914fb
martijn <martijn.brug@gmail.com>
2021-02-10 11:14:27 +0100

    WIP

--- packages/vendure-plugin-sendcloud/src/sendcloud.controller.ts
+++ packages/vendure-plugin-sendcloud/src/sendcloud.controller.ts
@@ -1,24 +1,16 @@
-import {Body, Controller, Req, Post, Headers} from '@nestjs/common';
+import {Body, Controller, Headers, Post, Req} from '@nestjs/common';
 import {Request} from 'express';
-import {ChannelService, OrderService} from '@vendure/core';
-import {Connection} from 'typeorm';
-import {PaymentStateMachine} from '@vendure/core/dist/service/helpers/payment-state-machine/payment-state-machine';
 import {SendcloudService} from './sendcloud.service';
 import {SendcloudClient} from './sendcloud.client';
 
 @Controller('sendcloud')
 export class SendcloudController {
-    constructor(private orderService: OrderService,
-                private connection: Connection,
-                private channelService: ChannelService,
-                private paymentStateMachine: PaymentStateMachine,
-                private sendcloudService: SendcloudService
-    ) {
+    constructor(private sendcloudService: SendcloudService) {
     }
 
     @Post('webhook')
     async webhook(@Req() req: Request, @Body() body: any, @Headers(SendcloudClient.signatureHeader) signature: string): Promise<void> {
-        if(!this.sendcloudService.client.isValidWebhook((req as any).rawBody, signature)) {
+        if (!this.sendcloudService.client.isValidWebhook((req as any).rawBody, signature)) {
             console.error(`Invalid signature in incoming Sendcloud webhook`, body);
         }
         // TODO
