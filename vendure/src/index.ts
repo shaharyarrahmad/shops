c280f56078e927a2a8ffb0d6809df1248e0cbc06
martijn <martijn.brug@gmail.com>
2021-02-10 11:02:24 +0100

    Test change

--- vendure/src/index.ts
+++ vendure/src/index.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import { bootstrap } from '@vendure/core';
 import { config } from './vendure-config';
+
 bootstrap(config)
     .then(() => {
         console.log(`\x1b[46mUsing database ${process.env.DATABASE_NAME} \x1b[0m`);
