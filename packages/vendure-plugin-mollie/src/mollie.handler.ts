efdfbab1a50a5634a120e8b0aa249a3064c53cd5
martijnvdbrug <martijn.brug@gmail.com>
2021-04-16 13:45:28 +0200

    updated to beta-9

--- packages/vendure-plugin-mollie/src/mollie.handler.ts
+++ packages/vendure-plugin-mollie/src/mollie.handler.ts
@@ -2,27 +2,27 @@ import {
   CreatePaymentResult,
   Logger,
   PaymentMethodHandler,
-  SettlePaymentResult,
-} from '@vendure/core';
-import { LanguageCode } from '@vendure/common/lib/generated-types';
-import createMollieClient from '@mollie/api-client';
-import { MolliePlugin } from './mollie.plugin';
+  SettlePaymentErrorResult, SettlePaymentResult
+} from "@vendure/core";
+import { LanguageCode } from "@vendure/common/lib/generated-types";
+import createMollieClient from "@mollie/api-client";
+import { MolliePlugin } from "./mollie.plugin";
 
 export const molliePaymentHandler = new PaymentMethodHandler({
-  code: 'mollie-payment-handler',
+  code: "mollie-payment-handler",
   description: [
     {
       languageCode: LanguageCode.en,
-      value: 'Mollie payment',
-    },
+      value: "Mollie payment"
+    }
   ],
   args: {
     apiKey: {
-      type: 'string',
+      type: "string"
     },
     redirectUrl: {
-      type: 'string',
-    },
+      type: "string"
+    }
   },
 
   /** This is called when the `addPaymentToOrder` mutation is executed */
@@ -35,54 +35,48 @@ export const molliePaymentHandler = new PaymentMethodHandler({
   ): Promise<CreatePaymentResult> => {
     try {
       let { apiKey, redirectUrl } = args;
-      if (redirectUrl && !redirectUrl.endsWith('/')) {
+      if (redirectUrl && !redirectUrl.endsWith("/")) {
         redirectUrl = `${redirectUrl}/`; // append slash if
       }
       const mollieClient = createMollieClient({ apiKey });
       const payment = await mollieClient.payments.create({
+        customerId: "",
+        mandateId: "",
         amount: {
           value: `${(order.totalWithTax / 100).toFixed(2)}`,
-          currency: 'EUR',
+          currency: "EUR"
         },
         metadata: {
-          orderCode: order.code,
+          orderCode: order.code
         },
         description: `Bestelling ${order.code}`,
         redirectUrl: `${redirectUrl}order/${order.code}`,
-        webhookUrl: `${MolliePlugin.host}/payments/mollie/${ctx.channel.token}`,
+        webhookUrl: `${MolliePlugin.host}/payments/mollie/${ctx.channel.token}`
       });
       return {
         amount: order.totalWithTax,
         transactionId: payment.id,
-        state: 'Authorized' as const,
+        state: "Authorized" as const,
         metadata: {
           public: {
-            redirectLink: payment.getPaymentUrl(),
-          },
-        },
+            redirectLink: payment.getPaymentUrl()
+          }
+        }
       };
     } catch (err) {
       Logger.error(err, MolliePlugin.context);
       return {
         amount: order.totalWithTax,
-        state: 'Declined' as const,
+        state: "Declined" as const,
         metadata: {
-          errorMessage: err.message,
-        },
+          errorMessage: err.message
+        }
       };
     }
   },
 
   /** This is called when the `settlePayment` mutation is executed */
   settlePayment: async (order, payment, args): Promise<SettlePaymentResult> => {
-    try {
-      // do something
-      return { success: true };
-    } catch (err) {
-      return {
-        success: false,
-        errorMessage: err.message,
-      };
-    }
-  },
+    return { success: true };
+  }
 });
