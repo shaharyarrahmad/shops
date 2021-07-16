a834fe0002e4d896f04913990beb194bedaf5c6b
martijnvdbrug <martijn.brug@gmail.com>
2021-07-16 14:30:25 +0200

    fix: another test with uppercasing loglevel

--- vendure/src/vendure-config.ts
+++ vendure/src/vendure-config.ts
@@ -8,33 +8,28 @@ import {
   ProductVariantEvent,
   VendureConfig,
   VendureLogger,
-} from "@vendure/core";
-import { EmailPlugin } from "@vendure/email-plugin";
-import { AssetServerPlugin } from "@vendure/asset-server-plugin";
-import { AdminUiPlugin } from "@vendure/admin-ui-plugin";
-import path from "path";
+} from '@vendure/core';
+import { EmailPlugin } from '@vendure/email-plugin';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import path from 'path';
 import {
   GoogleStoragePlugin,
   GoogleStorageStrategy,
-} from "vendure-plugin-google-storage-assets";
-import { CustomStockAllocationStrategy } from "./stock-allocation/custom-stock-allocation.strategy";
-import { ChannelConfigPlugin } from "./channel-config/channel-config.plugin";
-import { WebhookPlugin } from "vendure-plugin-webhook";
-import { channelAwareEmailHandlers } from "./channel-config/channel-aware-email.handlers";
-import { MolliePlugin } from "vendure-plugin-mollie";
-import { DutchPostalCodePlugin } from "vendure-plugin-dutch-postalcode";
-import { CloudTasksPlugin } from "vendure-plugin-google-cloud-tasks";
-import winston from "winston";
+} from 'vendure-plugin-google-storage-assets';
+import { CustomStockAllocationStrategy } from './stock-allocation/custom-stock-allocation.strategy';
+import { ChannelConfigPlugin } from './channel-config/channel-config.plugin';
+import { WebhookPlugin } from 'vendure-plugin-webhook';
+import { channelAwareEmailHandlers } from './channel-config/channel-aware-email.handlers';
+import { MolliePlugin } from 'vendure-plugin-mollie';
+import { DutchPostalCodePlugin } from 'vendure-plugin-dutch-postalcode';
+import { CloudTasksPlugin } from 'vendure-plugin-google-cloud-tasks';
+import { cloudLogger } from './logger';
 
 let logger: VendureLogger;
 if (process.env.K_SERVICE) {
   // This means we are in CloudRun
-  const { LoggingWinston } = require("@google-cloud/logging-winston");
-  const loggingWinston = new LoggingWinston();
-  logger = winston.createLogger({
-    level: "info",
-    transports: [new winston.transports.Console(), loggingWinston],
-  });
+  logger = cloudLogger;
 } else {
   logger = new DefaultLogger({ level: LogLevel.Debug });
 }
@@ -48,29 +43,29 @@ export const config: VendureConfig = {
   },
   apiOptions: {
     port: ((process.env.PORT as unknown) as number) || 3000,
-    adminApiPath: "admin-api",
+    adminApiPath: 'admin-api',
     adminApiPlayground: {}, // turn this off for production
     adminApiDebug: false, // turn this off for production
-    shopApiPath: "shop-api",
+    shopApiPath: 'shop-api',
     shopApiPlayground: {}, // turn this off for production
     shopApiDebug: false, // turn this off for production
   },
   authOptions: {
     superadminCredentials: {
-      identifier: "admin",
+      identifier: 'admin',
       password: process.env.SUPERADMIN_PASS as string,
     },
-    tokenMethod: "bearer",
+    tokenMethod: 'bearer',
   },
   dbConnectionOptions: {
-    type: "mysql",
+    type: 'mysql',
     synchronize: false,
     logging: false,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
-    migrations: [path.join(__dirname, "../migrations/*.ts")],
+    migrations: [path.join(__dirname, '../migrations/*.ts')],
   },
   paymentOptions: {
     paymentMethodHandlers: [],
@@ -80,13 +75,13 @@ export const config: VendureConfig = {
     CloudTasksPlugin.init({
       taskHandlerHost: process.env.VENDURE_HOST!,
       projectId: process.env.GOOGLE_PROJECT_ID!,
-      location: "europe-west1",
+      location: 'europe-west1',
       authSecret: process.env.CLOUD_TASKS_SECRET!,
       queueSuffix: process.env.QUEUE_SUFFIX,
     }),
     DutchPostalCodePlugin.init(process.env.POSTCODE_APIKEY as string),
     WebhookPlugin.init({
-      httpMethod: "POST",
+      httpMethod: 'POST',
       delay: 3000,
       events: [
         ProductEvent,
@@ -103,35 +98,35 @@ export const config: VendureConfig = {
         new GoogleStorageStrategy({
           bucketName: process.env.BUCKET!,
         }),
-      route: "assets",
-      assetUploadDir: "/tmp/vendure/assets",
+      route: 'assets',
+      assetUploadDir: '/tmp/vendure/assets',
     }),
     DefaultSearchPlugin,
     EmailPlugin.init({
       transport: {
-        type: "smtp",
-        host: "smtp.zoho.eu",
+        type: 'smtp',
+        host: 'smtp.zoho.eu',
         port: 587,
         secure: false,
         logging: false,
         debug: false,
         auth: {
-          user: "noreply@pinelab.studio",
+          user: 'noreply@pinelab.studio',
           pass: process.env.ZOHO_PASS as string,
         },
       },
       handlers: channelAwareEmailHandlers,
-      templatePath: path.join(__dirname, "../static/email/templates"),
+      templatePath: path.join(__dirname, '../static/email/templates'),
       globalTemplateVars: {
         fromAddress: '"Webshop" <noreply@pinelab.studio>',
       },
     }),
     // Production ready, precompiled admin UI
     AdminUiPlugin.init({
-      route: "admin",
+      route: 'admin',
       port: 3002,
       app: {
-        path: path.join(__dirname, "__admin-ui/dist"),
+        path: path.join(__dirname, '__admin-ui/dist'),
       },
     }),
   ],
