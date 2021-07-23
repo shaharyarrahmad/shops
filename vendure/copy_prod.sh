2bd14fbed048cc6fef99a470697746bba92d48f0
martijnvdbrug <martijn.brug@gmail.com>
2021-07-23 17:22:49 +0200

    Updated to 1.1.2

--- vendure/copy_prod.sh
+++
@@ -1,6 +0,0 @@
-# Copy assets
-# gsutil -m rsync -r -d -p gs://pinelab-shops-assets gs://pinelab-shops-test-assets
-
-# copy DB
-# mysqldump --column-statistics=0 -u *** -h *** -p *** > export.sql
-# mysql -u *** -h *** -p *** < export.sql
