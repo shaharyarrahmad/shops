118ddf96d47db6ca9bca856745e675bf58c9a0ad
martijnvdbrug <martijn.brug@gmail.com>
2021-07-29 17:10:04 +0200

    fix: only init mobile logging on client

--- clients/bendeboef/src/main.js
+++ clients/bendeboef/src/main.js
@@ -26,7 +26,7 @@ import '@fontsource/work-sans';
 import QuantityInput from 'pinelab-storefront-client/lib/buefy-components/QuantityInput';
 
 export default function (Vue, { router, head, isClient }) {
-  if (process.env.GRIDSOME_ENABLE_MOBILE_CONSOLE) {
+  if (isClient && process.env.GRIDSOME_ENABLE_MOBILE_CONSOLE) {
     require('outfront').default();
     console.log('OutfrontJS mobile logging enabled');
   }
