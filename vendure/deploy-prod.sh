253249bfa80741bbe928a4ec4cf2a06d930ac0fe
martijnvdbrug <martijn.brug@gmail.com>
2021-08-17 10:10:27 +0200

    fix(vendure): worker prod deploy

--- vendure/deploy-prod.sh
+++ vendure/deploy-prod.sh
@@ -9,3 +9,12 @@ gcloud run deploy shops-api \
             --memory=2G \
             --project=pinelab-shops \
             --set-env-vars=$ENV_VARS
+gcloud run deploy worker \
+            --quiet \
+            --image "eu.gcr.io/pinelab-shops/vendure:latest" \
+            --region "europe-west1" \
+            --platform "managed" \
+            --allow-unauthenticated \
+            --memory=2G \
+            --project=pinelab-shops \
+            --set-env-vars=$ENV_VARS
