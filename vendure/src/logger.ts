9006c85b3cb576ad91d263d99fcde09735f2fe9a
martijnvdbrug <martijn.brug@gmail.com>
2021-08-17 10:17:51 +0200

    fix(vendure): winston error stacktrace

--- vendure/src/logger.ts
+++ vendure/src/logger.ts
@@ -1,11 +1,17 @@
 import winston from 'winston';
 import { LoggingWinston } from '@google-cloud/logging-winston';
+import {format } from 'winston';
+const { combine, prettyPrint, errors } = format;
 
 const loggingWinston = new LoggingWinston({
   logName: `winston_${process.env.SHOP_ENV}`,
 });
 
 export const cloudLogger = winston.createLogger({
+  format: combine(
+    errors({ stack: true }), 
+    prettyPrint()
+  ),
   level: 'info',
   transports: [loggingWinston],
 });
