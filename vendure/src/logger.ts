a2acfaffa060cb112b96ddc6c19eb9944a88b15b
martijnvdbrug <martijn.brug@gmail.com>
2021-08-17 10:43:33 +0200

    fix(vendure): logname with servciename included

--- vendure/src/logger.ts
+++ vendure/src/logger.ts
@@ -1,17 +1,14 @@
 import winston from 'winston';
 import { LoggingWinston } from '@google-cloud/logging-winston';
-import {format } from 'winston';
+import { format } from 'winston';
 const { combine, prettyPrint, errors } = format;
 
 const loggingWinston = new LoggingWinston({
-  logName: `winston_${process.env.SHOP_ENV}`,
+  logName: `winston_${process.env.K_SERVICE}`,
 });
 
 export const cloudLogger = winston.createLogger({
-  format: combine(
-    errors({ stack: true }), 
-    prettyPrint()
-  ),
+  format: combine(errors({ stack: true }), prettyPrint()),
   level: 'info',
   transports: [loggingWinston],
 });
