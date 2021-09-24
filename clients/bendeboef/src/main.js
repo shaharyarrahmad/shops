fe4ed5250ae9697ac745029671a874d5fd3511e8
Martijn <martijn.brug@gmail.com>
2021-09-24 15:52:33 +0200

    feat(ben,supera): using popupimage from client lib

--- clients/bendeboef/src/main.js
+++ clients/bendeboef/src/main.js
@@ -24,6 +24,7 @@ import { configureVue } from 'pinelab-storefront-client';
 import '~/theme.scss';
 import '@fontsource/work-sans';
 import QuantityInput from 'pinelab-storefront-client/lib/buefy-components/QuantityInput';
+import PopupImage from 'pinelab-storefront-client/lib/buefy-components/PopupImage';
 
 export default function (Vue, { router, head, isClient }) {
   if (isClient && process.env.GRIDSOME_ENABLE_MOBILE_CONSOLE) {
@@ -52,6 +53,7 @@ export default function (Vue, { router, head, isClient }) {
   ].forEach((component) => Vue.use(component));
   Vue.component('QuantityInput', QuantityInput);
   Vue.component('Layout', Layout);
+  Vue.component('PopupImage', PopupImage);
   configureVue(Vue, { router, head, isClient });
 
   // Directus assets, use CMS host for local, otherwise go through netlify
