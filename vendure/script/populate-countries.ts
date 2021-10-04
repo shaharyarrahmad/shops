04bc2b8ace3ddc9ff49606f11cdd897b2f4f5f69
Martijn <martijn.brug@gmail.com>
2021-10-04 13:35:05 +0200

    feat(vendure): tax zones migration

---
+++ vendure/script/populate-countries.ts
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: process.env.LOCAL_ENV });
+import { bootstrap } from '@vendure/core';
+import { populate } from '@vendure/core/cli';
+import {config} from '../src/vendure-config';
+import {initialData} from './initial-data';
+
+populate(
+    () => bootstrap(config),
+    initialData,
+)
+    .then(app => {
+      console.log('Populated');
+        return app.close();
+    })
+    .then(
+        () => process.exit(0),
+        err => {
+            console.log(err);
+            process.exit(1);
+        },
+    );
