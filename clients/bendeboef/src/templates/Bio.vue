83a69d1f72300f2491199903ec940b8479b22c88
Martijn <martijn.brug@gmail.com>
2021-10-15 12:02:06 +0200

    feat: bio image

--- clients/bendeboef/src/templates/Bio.vue
+++ clients/bendeboef/src/templates/Bio.vue
@@ -2,6 +2,7 @@
   <Layout>
     <template #content>
       <h1 class="title">{{  $context.bio.title }}</h1>
+      <img class="bio-image" :src="getDefaultImage($context.bio.image.id)" :alt="$context.bio.image.title">
       <div v-html="$context.bio.text">
       </div>
     </template>
@@ -14,5 +15,10 @@ export default {};
 h2, h3, h4 {
   font-weight: bold;
   margin: 1rem 0 0.8rem 0;
+}
+.bio-image {
+  height: 300px;
+  width: 100%;
+  object-fit: cover;
 }
 </style>
