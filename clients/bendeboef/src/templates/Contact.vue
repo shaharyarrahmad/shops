6174dff6db1125afc2ed5b02ef1446c46c4faeb3
Martijn <martijn.brug@gmail.com>
2021-10-28 19:34:13 +0200

    fix(bendeboef): typo

--- clients/bendeboef/src/templates/Contact.vue
+++ clients/bendeboef/src/templates/Contact.vue
@@ -2,7 +2,7 @@
   <Layout>
     <template #content>
       <h1 class="title">Contact</h1>
-      <p>Contact me via Instagram <a :href="$context.global.instagram" target="_blank">@bendeboef</a></p>
+      <p>Contact me through Instagram <a :href="$context.global.instagram" target="_blank">@bendeboef</a></p>
     </template>
   </Layout>
 </template>
