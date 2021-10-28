ed5e95238bb703826bddaf32d4b6c5683925f183
Martijn <martijn.brug@gmail.com>
2021-10-28 19:00:25 +0200

    feat(bendeboef): analytics

--- clients/bendeboef/src/main.js
+++ clients/bendeboef/src/main.js
@@ -25,6 +25,7 @@ import '~/theme.scss';
 import '@fontsource/work-sans';
 import QuantityInput from 'pinelab-storefront-client/lib/buefy-components/QuantityInput';
 import PopupImage from 'pinelab-storefront-client/lib/buefy-components/PopupImage';
+import VueGtag from "vue-gtag";
 
 export default function (Vue, { router, head, isClient }) {
   if (isClient && process.env.GRIDSOME_ENABLE_MOBILE_CONSOLE) {
@@ -51,6 +52,16 @@ export default function (Vue, { router, head, isClient }) {
     Table,
     Tooltip,
   ].forEach((component) => Vue.use(component));
+  if (isClient) {
+    Vue.use(VueGtag, {
+      config: {
+        id: 'G-HWPW53KXT8',
+        params: {
+          anonymize_ip: true
+        }
+      }
+    }, router);
+  }
   Vue.component('QuantityInput', QuantityInput);
   Vue.component('Layout', Layout);
   Vue.component('PopupImage', PopupImage);
