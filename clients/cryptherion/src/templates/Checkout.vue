c8c5b67b3e8783d24b3672d6481828bf9da784d7
Martijn <martijn.brug@gmail.com>
2022-03-03 16:54:19 +0100

    feat(cryptherion): checkout dtuch

--- clients/cryptherion/src/templates/Checkout.vue
+++ clients/cryptherion/src/templates/Checkout.vue
@@ -3,6 +3,16 @@
     <CheckoutSteps
       previousPage="/cart/"
       :availableCountries="$context.availableCountries"
+      company-label="Bedrijfsnaam"
+      firstname-label="Voornaam"
+      lastname-label="Achternaam"
+      phone-label="Telefoonnr."
+      email-label="Emailadres"
+      postal-code-label="Postcode"
+      house-number-label="Huisnr."
+      street-label="Straat"
+      city-label="Plaats"
+      country-label="Land"
     />
   </Layout>
 </template>
