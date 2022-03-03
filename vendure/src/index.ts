7e03e8690c89bede6860b62589767854fc9af2f3
Martijn <martijn.brug@gmail.com>
2022-03-03 09:41:04 +0100

    feat(vendure): worker in main process on worker instances

--- vendure/src/index.ts
+++ vendure/src/index.ts
@@ -1,21 +1,15 @@
 require('dotenv').config({ path: process.env.LOCAL_ENV });
-import { bootstrap, bootstrapWorker, Logger } from '@vendure/core';
+import { bootstrap, JobQueueService, Logger } from '@vendure/core';
 import { config, runningInWorker } from './vendure-config';
 
-if (runningInWorker) {
-  bootstrapWorker(config)
-    .then(() => {
-      Logger.info(`Bootstrapped worker for env ${process.env.SHOP_ENV}`);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-} else {
-  bootstrap(config)
-    .then(() => {
-      Logger.info(`Bootstrapped Vendure for env ${process.env.SHOP_ENV}`);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
+bootstrap(config)
+  .then(async (app) => {
+    if (runningInWorker) {
+      Logger.info(`Started JobQueueService ${process.env.SHOP_ENV}`);
+      await app.get(JobQueueService).start();
+    }
+    Logger.info(`Bootstrapped Vendure for env ${process.env.SHOP_ENV}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
