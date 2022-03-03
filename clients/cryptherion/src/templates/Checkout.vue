27b479b64e131fdcca27d1f7493e766aac8c5b2a
Martijn <martijn.brug@gmail.com>
2022-03-03 17:00:22 +0100

    feat(cryptherion): checkout dtuch

--- clients/cryptherion/src/templates/Checkout.vue
+++ clients/cryptherion/src/templates/Checkout.vue
@@ -3,6 +3,10 @@
     <CheckoutSteps
       previousPage="/cart/"
       :availableCountries="$context.availableCountries"
+      customer-details-label="Adres gegevens"
+      shipping-label="Verzending"
+      payment-label="Betalen"
+      succes-label="Besteld!"
       company-label="Bedrijfsnaam"
       firstname-label="Voornaam"
       lastname-label="Achternaam"
@@ -13,6 +17,7 @@
       street-label="Straat"
       city-label="Plaats"
       country-label="Land"
+      total-label="Totaal"
     />
   </Layout>
 </template>
--- clients/cryptherion/src/templates/Order.vue
+++ clients/cryptherion/src/templates/Order.vue
@@ -1,6 +1,16 @@
 <template>
   <Layout>
-    <OrderConfirmation />
+    <OrderConfirmation
+      message="Gefeliciteerd met je bestelling! Je hebt een bevestiging in de mail gekregen."
+      item-label="Product"
+      quantity-label="Aantal"
+      total-label="Totaal"
+      customer-details-label="Adres gegevens"
+      shipping-label="Verzending"
+      payment-label="Betalen"
+      succes-label="Besteld!"
+      tax-label="BTW"
+    />
   </Layout>
 </template>
 <script>
