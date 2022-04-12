611719f86d10bdd7519cb2516b7447db3ab1db9f
Martijn <martijn.brug@gmail.com>
2022-04-12 16:51:52 +0200

    feat(op): Small changes

--- clients/op/src/templates/Index.vue
+++ clients/op/src/templates/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <section class="has-text-centered">
-      <b-button class="is-primary is-large" tag="a" href="#buy"
+      <b-button class="is-primary" tag="a" href="#buy"
         >Koop het e-boek voor
         <b>{{ $context.product.lowestPrice | euro }}</b>
       </b-button>
@@ -10,73 +10,84 @@
       <br />
     </section>
 
-    <section id="about" class="columns">
-      <div class="column">
-        <PopupImage
-          :small="$context.product.featuredAsset.preview"
-          :alt="$context.product.name"
-          :large="$context.product.featuredAsset.preview"
-          class="mb-4"
-        />
-        <div class="columns is-mobile">
-          <div class="column" v-for="asset of $context.product.assets">
-            <PopupImage
-              :small="asset.thumbnail"
-              :alt="$context.product.name"
-              :large="asset.preview"
-              class="mb-4"
-            />
+    <section id="about">
+      <h2 class="has-text-centered">{{ $context.product.name }}</h2>
+      <br />
+      <div class="columns">
+        <div class="column">
+          <PopupImage
+            :small="$context.product.featuredAsset.preview"
+            :alt="$context.product.name"
+            :large="$context.product.featuredAsset.preview"
+            class="mb-4"
+          />
+          <div class="columns is-mobile">
+            <div class="column" v-for="asset of $context.product.assets">
+              <PopupImage
+                :small="asset.thumbnail"
+                :alt="$context.product.name"
+                :large="asset.preview"
+                class="mb-4"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="column">
-        <div class="description">
-          <h2>{{ $context.product.name }}</h2>
-          <div
-            class="is-family-secondary"
-            v-html="$context.product.description"
-          ></div>
-        </div>
-        <br />
-        <br />
-        <h2 class="has-text-primary">
-          Koop het e-boek hier voor {{ $context.product.lowestPrice | euro }}
-        </h2>
-        <div id="buy">
-          <p>
-            Vul je emailadres in, reken af en ontvang het boek direct in je
-            email!
-          </p>
-          <form v-on:submit="buy()">
-            <b-field grouped label="Emailadres" label-position="on-border">
-              <b-input type="email" v-model="emailAddress" expanded></b-input>
-              <span class="control">
-                <b-button
-                  tag="input"
-                  native-type="submit"
-                  value="Koop het boek"
-                  type="is-primary"
-                  :loading="loading"
-                />
-              </span>
-            </b-field>
-            <img
-              src="/img/mollie.png"
-              alt="Veilig betalen via Mollie"
-              class="is-shadowless"
-              style="height: 50px; opacity: 0.7"
-            />
-          </form>
+        <div class="column">
+          <div class="description">
+            <div
+              class="is-family-secondary"
+              v-html="$context.product.description"
+            ></div>
+          </div>
+          <br />
+          <br />
+          <div id="buy">
+            <h2 class="has-text-primary">
+              Koop het e-boek hier voor
+              {{ $context.product.lowestPrice | euro }}
+            </h2>
+            <p>Betaal online en ontvang het boek direct in je email!</p>
+            <form v-on:submit="buy($event)">
+              <b-field grouped label="Emailadres" label-position="on-border">
+                <b-input
+                  type="email"
+                  v-model="emailAddress"
+                  expanded
+                  required
+                ></b-input>
+                <span class="control">
+                  <b-button
+                    tag="input"
+                    native-type="submit"
+                    value="Koop het boek"
+                    type="is-primary"
+                    :loading="loading"
+                  />
+                </span>
+              </b-field>
+              <img
+                src="/img/mollie.png"
+                alt="Veilig betalen via Mollie"
+                class="is-shadowless"
+                style="height: 50px; opacity: 0.7"
+              />
+            </form>
+          </div>
         </div>
       </div>
     </section>
 
-    <section id="faq">
+    <section id="faq" class="mt-6">
       <h2 class="has-text-primary">FAQ / Veelgestelde vragen</h2>
       <p>
         <b>Klopt het dat ... ?</b><br />
         Nee!
+      </p>
+      <p>
+        <b>Wat kost het boek?</b><br />
+        Voor {{ $context.product.lowestPrice | euro }} ontvang je de PDF in je
+        mail!
       </p>
     </section>
 
@@ -93,15 +104,11 @@ export default {
     };
   },
   methods: {
-    async buy() {
+    async buy(event) {
       try {
         this.loading = true;
-        if (!this.emailAddress) {
-          this.showError(`Vul een geldig emailadres in`);
-          return;
-        }
         // TODO empty cart
-        // Add 1 to cart
+        // TODO Add 1 to cart
         await this.$vendure.setCustomerForOrder({
           firstName: this.emailAddress,
           lastName: 'e-book',
