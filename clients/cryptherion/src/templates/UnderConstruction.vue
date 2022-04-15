7c2b1008603181ca015bd0247364d0229183c72a
Martijn <martijn.brug@gmail.com>
2022-04-15 08:29:39 +0200

    feat(cryptherion): pause untill mei first

--- clients/cryptherion/src/templates/UnderConstruction.vue
+++ clients/cryptherion/src/templates/UnderConstruction.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="content" v-html="$context.content"></div>
+  <div class="container is-widescreen section has-text-centered">
+    <img
+      src="/img/cryptherion-logo-black.svg"
+      alt="Cryptherion logo"
+      style="max-height: 100px"
+    />
+    <br />
+    <br />
+    <div class="content" v-html="$context.content"></div>
+  </div>
 </template>
 <script>
 export default {};
