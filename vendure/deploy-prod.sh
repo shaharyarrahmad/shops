3553b781124232f071ca8de3b62b99d330a55950
Martijn <martijn.brug@gmail.com>
2022-04-19 17:01:15 +0200

    feat(vendure): prod socket

--- vendure/deploy-prod.sh
+++ vendure/deploy-prod.sh
@@ -9,6 +9,7 @@ gcloud run deploy shops-api \
             --memory=2G \
             --project=pinelab-shops \
             --set-env-vars=$ENV_VARS
+            --add-cloudsql-instances="pinelab-shops:europe-west1:shops-prod"
 gcloud run deploy worker \
             --quiet \
             --image "eu.gcr.io/pinelab-shops/vendure:latest" \
@@ -18,3 +19,4 @@ gcloud run deploy worker \
             --memory=2G \
             --project=pinelab-shops \
             --set-env-vars=$ENV_VARS
+            --add-cloudsql-instances="pinelab-shops:europe-west1:shops-prod"
