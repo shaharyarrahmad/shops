811de2480846f86786062484c80da7c49a7d5ed3
Martijn <martijn.brug@gmail.com>
2022-05-31 11:25:25 +0200

    feat(vendure): added admin ui buttons from plugin

--- vendure/src/compile-admin-ui.ts
+++ vendure/src/compile-admin-ui.ts
@@ -5,6 +5,10 @@ import { MyparcelPlugin } from 'vendure-plugin-myparcel/dist/myparcel.plugin';
 import { GoedgepicktPlugin } from 'vendure-plugin-goedgepickt';
 import { InvoicePlugin } from 'vendure-plugin-invoices';
 import { EBoekhoudenPlugin } from 'vendure-plugin-e-boekhouden';
+import {
+  cancelOrderButton,
+  completeOrderButton,
+} from 'vendure-plugin-admin-ui-helpers';
 
 compileUiExtensions({
   outputPath: path.join(__dirname, '__admin-ui'),
@@ -14,6 +18,8 @@ compileUiExtensions({
     MyparcelPlugin.ui,
     GoedgepicktPlugin.ui,
     EBoekhoudenPlugin.ui,
+    cancelOrderButton,
+    completeOrderButton,
     setBranding({
       // The small logo appears in the top left of the screen
       smallLogoPath: path.join(__dirname, 'admin-branding/favicon.png'),
