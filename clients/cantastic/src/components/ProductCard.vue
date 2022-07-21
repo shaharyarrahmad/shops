75580488479eae3dddb51b2f8354bb6fda073939
Martijn <martijn.brug@gmail.com>
2022-07-21 11:57:29 +0200

    fix(cantastic): gradient bannmer

--- clients/cantastic/src/components/ProductCard.vue
+++ clients/cantastic/src/components/ProductCard.vue
@@ -118,6 +118,11 @@ export default {
   line-height: 40px;
   border-radius: 50%;
   text-align: center;
-  /*background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);*/
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 50%,
+    rgba(252, 176, 69, 1) 100%
+  );
 }
 </style>
