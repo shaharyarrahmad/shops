d8c4cf0c14380c762bbacfb10bcf68736a749605
Martijn <martijn.brug@gmail.com>
2022-07-25 15:54:13 +0200

    fix: print gecko

--- packages/e2e/nightwatch.conf.js
+++ packages/e2e/nightwatch.conf.js
@@ -5,7 +5,7 @@ module.exports = {
 
   webdriver: {
     start_process: true,
-    check_process_delay: 90000,
+    check_process_delay: 3000,
     port: 4444,
     server_path: require('geckodriver').path,
     cli_args: [
