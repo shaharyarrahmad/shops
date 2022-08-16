afd316591d9150b182da99c03e0c96af455f53ee
Martijn <martijn.brug@gmail.com>
2022-08-16 17:07:58 +0200

    fix(cantastic): ui labels

--- vendure/src/invoice/tax-invoice-strategy.ts
+++ vendure/src/invoice/tax-invoice-strategy.ts
@@ -6,6 +6,7 @@ export class TaxInvoiceStrategy extends DefaultDataStrategy {
   async getData(input: DataFnInput): Promise<InvoiceData> {
     const defaultData = await super.getData(input);
     const summary = TaxHelper.getTaxSummary(input.order);
+    // eBoekhouden calculates priceExBTW back from priceIncBTW to prevent rounding errors
     const eboekhoudenSummary = TaxHelper.getEBoekhoudenTaxSummary(input.order);
     return {
       ...defaultData,
