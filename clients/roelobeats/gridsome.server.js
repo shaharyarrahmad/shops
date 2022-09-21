2652ab04a2508640bbee1391703340cc76a0bf44
roelobeats <raul.santing@gmail.com>
2022-09-21 16:06:02 +0200

    feat(highlighted)

--- clients/roelobeats/gridsome.server.js
+++ clients/roelobeats/gridsome.server.js
@@ -10,13 +10,40 @@ module.exports = async function (api) {
       await vendureServer.getShopData();
       collections = vendureServer.unflatten(collections);
 
-    console.log(JSON.stringify(products));
+    const highlight1 = products.find(p => {
+      return p.facetValues.find(f => f.code === 'highlights-1')
+    })
+
+    const highlight2 = products.find(p => {
+      return p.facetValues.find(f => f.code === 'highlights-2')
+    })
+
+    const highlight3 = products.find(p => {
+      return p.facetValues.find(f => f.code === 'highlights-3')
+    })
+
+    products.forEach(p => {
+      p.shortDescription = p.description.substring(0, 75) + "...";
+    })
+
+    products.forEach(p => {
+      p.mediumDescription = p.description.substring(0, 150) + "...";
+    })
+
+    products.forEach(p => {
+      p.largeDescription = p.description.substring(0, 225) + "...";
+    })
+
+
+
 
     createPage({
       path: '/',
       component: './src/templates/Index.vue',
       context: {
         products, 
+        highlight1, highlight2, highlight3,
+        topproducts: products.slice(0,5),
         categories: collections,
       },
     });
--- clients/roelobeats/src/templates/Index.vue
+++ clients/roelobeats/src/templates/Index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
    <AppHeader />
+   <Highlighted />
    <AppFooter />
+
   </div>
  </template>
 
@@ -9,11 +11,14 @@
 <script>
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
+import Highlighted from '@/components/Highlighted.vue';
+
 
 export default {
   components: {
     AppHeader,
     AppFooter,
+    Highlighted,
   },
 };
 </script>
