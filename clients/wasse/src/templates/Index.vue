e0222209b6ee7617298d4318b5523b7b8a54e9ee
Martijn <martijn.brug@gmail.com>
2022-10-03 10:28:00 +0200

    feat(wasse): structure setup

---
+++ clients/wasse/src/templates/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <DefaultLayout>
+    <h1>Welkom bij Wormenkwekerij Wasse</h1>
+
+    <!---- example category cards --->
+    <div class="columns">
+      <template v-for="category of 5">
+        <div class="column">
+          <ExampleComponent :title="`Title ${category}`">
+            <PopupImage
+              small="https://wormenkwekerijwasse.nl/logo-wormenkwekerijwasse.png"
+              large="https://wormenkwekerijwasse.nl/logo-wormenkwekerijwasse.png"
+            />
+          </ExampleComponent>
+        </div>
+      </template>
+    </div>
+  </DefaultLayout>
+</template>
+<script>
+import ExampleComponent from '../components/ExampleComponent';
+export default {
+  components: { ExampleComponent },
+};
+</script>
