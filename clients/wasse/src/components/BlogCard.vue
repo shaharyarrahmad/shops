a894dcfe4559e9f8a5f10840989ac828f092ff8b
roelobeats <raul.santing@gmail.com>
2022-10-10 11:11:59 +0200

    feat(wasse): added blogcard & wassehomebutton

---
+++ clients/wasse/src/components/BlogCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <g-link :to="`/blog/${title.slug}`">
+    <div class="card container-overlay">
+      <div class="card-image">
+        <figure class="image is-3by2">
+          <img :src="image" />
+        </figure>
+        <div class="overlay"></div>
+      </div>
+      <div class="card-content has-background-success">
+        <div class="media">
+          <div class="media-content">
+            <p class="title has-text-white has-text-weight-bold is-5">
+              {{ title }}
+            </p>
+            <WasseHomeButton>Lees meer</WasseHomeButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  </g-link>
+</template>
+<script>
+import WasseHomeButton from '@/components/WasseHomeButton.vue';
+
+export default {
+  props: ['title', 'title.slug', 'image'],
+  components: { WasseHomeButton },
+};
+</script>
+<style>
+.card-content {
+  bottom: 0;
+  z-index: 1;
+  position: absolute;
+  width: 100%;
+}
+</style>
