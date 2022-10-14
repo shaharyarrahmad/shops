9ca70356a1310eb34331de7c063fbe8db841dc90
roelobeats <raul.santing@gmail.com>
2022-10-14 15:44:40 +0200

    fix(Wasse): minor changes

--- clients/wasse/src/templates/Index.vue
+++ clients/wasse/src/templates/Index.vue
@@ -1,14 +1,44 @@
 <template>
-  <AppHeader />
+    <DefaultLayout>
+    <!-- PRODUCT CARD -->
+    <section id="highlighted">
+      <div class="columns">
+        <div class="column">
+          <HighlightCard
+            title="Alle benodigdheden om te starten met kweken"
+            image="https://storage.googleapis.com/wassets/preview/ba/seeds__preview.jpeg"
+            slug="alle-benodigdheden-om-te-starten-met-kweken"
+          />
+        </div>
+        <div class="column is-4">
+          <div class="columns is-mobile is-multiline">
+            <div class="column is-12-tablet is-6-mobile">
+              <HighlightCard
+                title="Alle potgrond 1+1"
+                image="https://storage.googleapis.com/wassets/preview/ba/seeds__preview.jpeg"
+                slug="alle-potgrond-1+1"
+              />
+            </div>
+            <div class="column is-12-tablet is-6-mobile">
+              <HighlightCard
+                title="Nieuw in het assortiment"
+                image="https://storage.googleapis.com/wassets/preview/be/compost-rol-verdeler-2-min-300x300__preview.jpg"
+                slug="nieuw-in-het-assortiment"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </DefaultLayout>
 </template>
 <script>
 import ProductCard from '@/components/ProductCard.vue';
 import CategoryCard from '@/components/CategoryCard.vue';
 import BlogCard from '@/components/BlogCard.vue';
 import HighlightCard from '@/components/HighlightCard.vue';
-import AppHeader from '@/components/AppHeader.vue';
 export default {
-  components: { ProductCard, CategoryCard, BlogCard, HighlightCard, AppHeader },
+  components: { ProductCard, CategoryCard, BlogCard, HighlightCard },
 };
 </script>
 <style></style>
