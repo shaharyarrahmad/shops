571c00acac0d8fdfd4b0688c946479290ad1b8c6
roelobeats <raul.santing@gmail.com>
2022-10-18 11:12:06 +0200

    feat(wasse): added pop cat & product

--- clients/wasse/src/templates/Index.vue
+++ clients/wasse/src/templates/Index.vue
@@ -1,35 +1,64 @@
 <template>
-    <DefaultLayout>
+  <DefaultLayout>
     <!-- PRODUCT CARD -->
-    <section id="highlighted">
-      <div class="columns">
-        <div class="column">
-          <HighlightCard
-            title="Alle benodigdheden om te starten met kweken"
-            image="https://storage.googleapis.com/wassets/preview/ba/seeds__preview.jpeg"
-            slug="alle-benodigdheden-om-te-starten-met-kweken"
-          />
-        </div>
-        <div class="column is-4">
-          <div class="columns is-mobile is-multiline">
-            <div class="column is-12-tablet is-6-mobile">
-              <HighlightCard
-                title="Alle potgrond 1+1"
-                image="https://storage.googleapis.com/wassets/preview/ba/seeds__preview.jpeg"
-                slug="alle-potgrond-1+1"
-              />
-            </div>
-            <div class="column is-12-tablet is-6-mobile">
-              <HighlightCard
-                title="Nieuw in het assortiment"
-                image="https://storage.googleapis.com/wassets/preview/be/compost-rol-verdeler-2-min-300x300__preview.jpg"
-                slug="nieuw-in-het-assortiment"
-              />
+      <section id="highlighted">
+        <div class="columns">
+          <div class="column">
+            <HighlightCard
+              title="Alle benodigdheden om te starten met kweken"
+              image="https://storage.googleapis.com/wassets/preview/ba/seeds__preview.jpeg"
+              slug="alle-benodigdheden-om-te-starten-met-kweken"
+            />
+          </div>
+          <div class="column is-4">
+            <div class="columns is-mobile is-multiline">
+              <div class="column is-12-tablet is-6-mobile">
+                <HighlightCard
+                  title="Alle potgrond 1+1"
+                  image="https://storage.googleapis.com/wassets/preview/ba/seeds__preview.jpeg"
+                  slug="alle-potgrond-1+1"
+                />
+              </div>
+              <div class="column is-12-tablet is-6-mobile">
+                <HighlightCard
+                  title="Nieuw in het assortiment"
+                  image="https://storage.googleapis.com/wassets/preview/be/compost-rol-verdeler-2-min-300x300__preview.jpg"
+                  slug="nieuw-in-het-assortiment"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section id="popular-categories">
+        <h4 class="title has-text-black has-text-weight-bold is-5 pt-5">Populaire categorieën</h4>
+        <div class="columns is-multiline is-mobile">
+          <template v-for="index in 5">
+            <div class="column is-4-mobile is-one-fifth-desktop">
+              <CategoryCard
+                title="Boeken worm"
+                image="https://storage.googleapis.com/wassets/preview/36/ebooks__preview.jpeg"
+                slug="boeken-en-ebooks"
+              />
+            </div>
+          </template>
+        </div>
+      </section>
+      <section id="popular-products">
+        <h4 class="title has-text-black has-text-weight-bold is-5 pt-5">Populaire producten</h4>
+        <div class="columns is-multiline is-mobile">
+          <template v-for="index in 5">
+            <div class="column is-4-mobile is-one-fifth-desktop">
+              <ProductCard
+                title="Wormenkwekerij stickers"
+                image="https://storage.googleapis.com/wassets/preview/36/ebooks__preview.jpeg"
+                slug="wormenkwekerij-stickers"
+                price="14900"
+              />
+            </div>
+          </template>
+        </div>
+      </section>
   </DefaultLayout>
 </template>
 <script>
