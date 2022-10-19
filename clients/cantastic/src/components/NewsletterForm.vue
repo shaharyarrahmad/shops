77851b086f6d56653b955dd125a8b865541c816a
Martijn <martijn.brug@gmail.com>
2022-10-19 14:04:34 +0200

    feat(cantastic): show newsletter when no categories

---
+++ clients/cantastic/src/components/NewsletterForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form
+    action="https://cantastic.us15.list-manage.com/subscribe/post?u=4e226c3fa7ab1fc8dad78c479&amp;id=73fb352924"
+    method="post"
+    id="mc-embedded-subscribe-form"
+    name="mc-embedded-subscribe-form"
+    class="validate"
+    target="_blank"
+    novalidate
+  >
+    <b-field
+      message="Geen spam - 1 mail per maand. Je kunt je altijd weer afmelden."
+    >
+      <b-input
+        id="mce-EMAIL"
+        placeholder="Vul je e-mailadres in"
+        icon="email"
+        size="is-small"
+        type="email"
+        name="EMAIL"
+      >
+      </b-input>
+      <p class="control">
+        <b-button
+          id="mc-embedded-subscribe"
+          type="is-dark"
+          native-type="submit"
+          label="Count me in"
+          size="is-small"
+        />
+      </p>
+    </b-field>
+  </form>
+</template>
