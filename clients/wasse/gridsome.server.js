a1c0d03508a9d604670e7c45c40d55ee2e3032c3
Martijn <martijn.brug@gmail.com>
2022-10-21 10:06:02 +0200

    feat(wasse): collection urls

--- clients/wasse/gridsome.server.js
+++ clients/wasse/gridsome.server.js
@@ -43,7 +43,6 @@ module.exports = async function (api) {
     const global = {
       navbarCollections,
     };
-    console.log(navbarCollections);
 
     // -------------------- Home -----------------------------------
     createPage({
