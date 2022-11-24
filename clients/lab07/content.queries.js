5e27048713d15590476a3d65d9364592a0698372
Martijn <martijn.brug@gmail.com>
2022-11-24 15:54:13 +0100

    feat(lab07): fetch projects from directus cms

--- clients/lab07/content.queries.js
+++ clients/lab07/content.queries.js
@@ -1,7 +1,20 @@
 const gql = require(`graphql-tag`);
 
 // TODO define queries for fetching portfolio projects const GET_CONTENT = gql``;
-const GET_CONTENT = '';
+const GET_CONTENT = gql`
+  {
+    lab07_projects {
+      title
+      description
+      images {
+        directus_files_id {
+          id
+          title
+        }
+      }
+    }
+  }
+`;
 
 module.exports = {
   GET_CONTENT,
